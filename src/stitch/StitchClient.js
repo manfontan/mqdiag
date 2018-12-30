dc8b0178e74966457c9a1eed45c24525b1327a4f
Manuel Fontan <manuel.fontan@mongodb.com>
2018-12-30 16:15:13 +0100

    updated folder structure

---
+++ src/stitch/StitchClient.js
@@ -0,0 +1,15 @@
+import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+
+//Replate your-stitch-app with your stitch application id:
+Stitch.initializeDefaultAppClient('mqdiag-ryglf');
+const client = Stitch.defaultAppClient;
+
+client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+  console.log(`logged in anonymously as user ${user.id}`)
+});
+
+export function uploadExplain(arg){
+  return client.callFunction("postExplain", [arg]).then(diagnostics => {
+    return diagnostics;
+})
+};
