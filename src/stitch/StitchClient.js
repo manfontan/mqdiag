3bbe2f28a2621baeb8b678ae98a93172ccf6e041
Manuel Fontan <manuel.fontan@mongodb.com>
2018-12-31 21:41:55 +0100

    added Google OAuth logic

--- src/stitch/StitchClient.js
+++ src/stitch/StitchClient.js
@@ -1,15 +1,37 @@
-import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+import {
+  Stitch,
+  GoogleRedirectCredential
+} from 'mongodb-stitch-browser-sdk';
 
 //Replate your-stitch-app with your stitch application id:
-Stitch.initializeDefaultAppClient('mqdiag-ryglf');
-const client = Stitch.defaultAppClient;
+var client;
 
-client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
-  console.log(`logged in anonymously as user ${user.id}`)
-});
+if(!client){
+  Stitch.initializeDefaultAppClient('mqdiag-ryglf');
+}
+client = Stitch.defaultAppClient;
 
-export function uploadExplain(arg){
+let authed = client.auth.isLoggedIn;
+
+console.log("redirect result: " + client.auth.hasRedirectResult());
+if (client.auth.hasRedirectResult()) {
+  client.auth.handleRedirectResult().then(user => {
+    console.log(user);
+  });
+}
+
+console.log("user logged status: " + authed);
+if (!authed) {
+  const credential = new GoogleRedirectCredential();
+  client.auth.loginWithRedirect(credential);
+}
+
+if(authed){
+  console.log(client.auth.user.profile.email);
+}
+
+export function uploadExplain(arg) {
   return client.callFunction("postExplain", [arg]).then(diagnostics => {
     return diagnostics;
-})
+  })
 };
